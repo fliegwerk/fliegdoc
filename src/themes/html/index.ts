4865659fcd3e069ccf4e174814ae9688e4c928c4
rxliuli <rxliuli@gmail.com>
2021-01-18 23:55:36 +0800

    Update the minimum supported nodejs version to v10 and fix bugs Note: May contain some bugs, mainly due to two reasons 1. replacing replaceAll with replace 2. replace ? for ||
    There are also some non-dangerous replacements
    For example, appending the fs-extra module to Promise fs-related operations

--- src/themes/html/index.ts
+++ src/themes/html/index.ts
@@ -1,6 +1,6 @@
 import { Theme } from '../../model';
 import MarkdownIt from 'markdown-it';
-import * as fsp from 'fs/promises';
+import * as fsp from 'fs-extra';
 import * as fs from 'fs';
 import * as path from 'path';
 import { renderFile } from 'eta';
@@ -15,9 +15,7 @@ const viewFolder = path.resolve(__dirname, '..', '..', '..', 'views');
  */
 const md = {
 	render: (md?: string) => {
-		return origMd.render(
-			(md ?? '').replaceAll(/{@link (.*?)}/g, '[`$1`](#$1)')
-		);
+		return origMd.render((md ?? '').replace(/{@link (.*?)}/g, '[`$1`](#$1)'));
 	}
 };
 
