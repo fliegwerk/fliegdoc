fliegwerk/fliegdoc

commit be6baffca4bf663c9f6664397f319c659e162059

refactor(html-theme): Extract `getSearchIndex` into multiple smaller methods (#19)
This increases the code readability.

Also fixes file name of `search-index.ts`

Co-authored-by: Ludwig Richter <richter@fliegwerk.com>
Closes: #18

src/themes/html/search-index.ts
@@ -1,0 +1,196 @@
+import { Tree, ModuleTreeNode, FliegdocConfig } from '../../model';
+import {
+	ClassDeclarationStructure,
+	InterfaceDeclarationStructure
+} from 'ts-morph';
+
+/**
+ * Creates a search index for the passed {@link Tree}
+ *
+ * @param tree - the tree for which the index gets generated
+ * @param config - the config that gets used
+ * @returns the search index, containing all linked members of the docs
+ *
+ * @example
+ * ```ts
+ * console.log(getSearchIndex(tree))
+ * ```
+ */
+export function getSearchIndex(
+	tree: Tree,
+	config: FliegdocConfig
+): SearchResult[] {
+	const res: SearchResult[] = [];
+
+	Object.keys(tree).forEach(moduleName => {
+		res.push({
+			name: moduleName,
+			text: moduleName,
+			url: `${config.baseUrl}${moduleName}`
+		});
+		const moduleTree = tree[moduleName];
+		res.push(...getSearchIndexForModuleMembers(moduleTree, moduleName, config));
+	});
+
+	return res;
+}
+
+/**
+ * Creates a search index for all members of the passed module node
+ *
+ * @param moduleTree - the module tree node
+ * @param moduleName - the module's name
+ * @param config - the current config with which the search index gets created
+ * @returns the search index of the class members
+ *
+ * @example
+ * ```ts
+ * const searchIndex = []
+ * searchIndex.push(...getSearchIndexForModuleMembers(
+ * 	moduleTree,
+ * 	`${moduleName}`
+ * 	config
+ * ));
+ * ```
+ */
+function getSearchIndexForModuleMembers(
+	moduleTree: (Record<string, unknown> & {
+		name: string;
+		declarations: ModuleTreeNode[];
+	})[],
+	moduleName: string,
+	config: FliegdocConfig
+): SearchResult[] {
+	const res: SearchResult[] = [];
+
+	moduleTree.forEach(moduleMember => {
+		res.push({
+			name: moduleMember.name,
+			text: moduleName + '.' + moduleMember.name,
+			url: `${config.baseUrl}${moduleName}#${moduleMember.name}`
+		});
+		res.push(
+			...getSearchIndexForModuleMemberChildren(
+				moduleMember.declarations,
+				moduleName + '.' + moduleMember.name,
+				config
+			)
+		);
+	});
+
+	return res;
+}
+
+/**
+ * Creates a search index for all children of a module member's declarations.
+ *
+ * @param declarations - the declarations of the module member
+ * @param prefix - the prefix of the module, e.g., `[module-name].[member-name]`
+ * @param config - the current config with which the search index gets created
+ * @returns the search index for all children of the `declarations`
+ *
+ * @example
+ * ```ts
+ * index.push(
+ * 	..getSearchIndexForModuleMemberChildren(
+ * 	moduleMember.declarations,
+ * 	moduleName + '.' + moduleMember.name,
+ * 	config
+ * );
+ * ```
+ */
+function getSearchIndexForModuleMemberChildren(
+	declarations: ModuleTreeNode[],
+	prefix: string,
+	config: FliegdocConfig
+) {
+	const res: SearchResult[] = [];
+
+	for (const node of declarations) {
+		if (
+			isType<ClassDeclarationStructure>(node, 'class') ||
+			isType<InterfaceDeclarationStructure>(node, 'interface')
+		) {
+			res.push(
+				...getSearchResultsForInterfaceAndClassMembers(node, prefix, config)
+			);
+		}
+	}
+
+	return res;
+}
+
+/**
+ * Creates a search index for all members of the passed interface or class declaration node
+ *
+ * @param node - the interface declaration node
+ * @param prefix - the module for the absolute name
+ * @param config - the current config with which the search index gets created
+ * @returns the search index of the interface members
+ *
+ * @example
+ * ```ts
+ * const searchIndex = []
+ * if (isInterfaceDeclaration(interfaceNode) || isClassDeclaration(InterfaceNode))
+ * 	searchIndex.push(...getSearchResultsForInterfaceAndClassMembers(
+ * 		interfaceNode,
+ * 		`${moduleName}.${$moduleMember.name}`,
+ * 		config
+ * 	));
+ * ```
+ */
+function getSearchResultsForInterfaceAndClassMembers(
+	node: ModuleTreeNode<
+		InterfaceDeclarationStructure | ClassDeclarationStructure
+	>,
+	prefix: string,
+	config: FliegdocConfig
+): SearchResult[] {
+	const res: SearchResult[] = [];
+
+	const interfaceDeclaration = node.declarations[0];
+	const memberToSearchResult = (property: any) => {
+		res.push({
+			name: property.name,
+			text: `${prefix}.${property.name}`,
+			url: `${config.baseUrl}${prefix}.${property.name}`
+		});
+	};
+	interfaceDeclaration.properties?.forEach(memberToSearchResult);
+	interfaceDeclaration.methods?.forEach(memberToSearchResult);
+
+	return res;
+}
+
+/**
+ * Checks if the given node is a {@link ModuleTreeNode} contains a class declaration
+ *
+ * @param node - the node
+ * @param typeName - the `node.type` for the type T
+ * @typeparam T - the type of `node.declarations[0]`, if `node.type === typeName`
+ * @returns does `node.declarations` contain a class declaration?
+ *
+ * @example
+ * ```ts
+ * if (isType<ClassDeclarationStructure>(node, 'class')) {
+ *     // node.declarations[0] is a ClassDeclarationStructure
+ * } else if (isType<InterfaceDeclarationStructure>(node, 'Interface')) {
+ *     // node.declarations[0] is a InterfaceDeclarationStructure
+ * }
+ * ```
+ */
+function isType<T>(
+	node: ModuleTreeNode,
+	typeName: string
+): node is ModuleTreeNode<T> {
+	return node.type === typeName;
+}
+
+/**
+ * A search result.
+ */
+export interface SearchResult {
+	name: string;
+	url: string;
+	text: string;
+}

.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
 		'tsdoc/syntax': 'error',
 		'prettier/prettier': 'warn',
 		'@typescript-eslint/no-inferrable-types': 0,
+		'jsdoc/check-tag-names': 0, // done by tsdoc checker
 		'jsdoc/require-jsdoc': [
 			'error',
 			{
@@ -37,7 +38,8 @@ module.exports = {
 		'jsdoc/require-param-type': 0,
 		'jsdoc/require-property-type': 0,
 		'jsdoc/require-returns-type': 0,
-		'jsdoc/require-example': 2
+		'jsdoc/require-example': 2,
+		'@typescript-eslint/no-explicit-any': 0
 	},
 	ignorePatterns: ['node_modules', 'docs', 'build']
 };

src/themes/html/index.ts
@@ -5,7 +5,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { renderFile } from 'eta';
 import { getConfig } from '../../model/config';
-import { getSearchIndex } from './serach-index';
+import { getSearchIndex } from './search-index';
 
 const origMd = new MarkdownIt({ linkify: true });
 const viewFolder = path.resolve(__dirname, '..', '..', '..', 'views');

src/themes/html/serach-index.ts
@@ -1,126 +1,0 @@
-import { Tree, ModuleTreeNode, FliegdocConfig } from '../../model';
-import {
-	ClassDeclarationStructure,
-	InterfaceDeclarationStructure
-} from 'ts-morph';
-
-/**
- * Creates a search index for the passed {@link Tree}
- *
- * @param tree - the tree for which the index gets generated
- * @param config - the config that gets used
- * @returns the search index, containing all linked members of the docs
- *
- * @example
- * ```ts
- * console.log(getSearchIndex(tree))
- * ```
- */
-export function getSearchIndex(
-	tree: Tree,
-	config: FliegdocConfig
-): SearchResult[] {
-	const res: SearchResult[] = [];
-
-	Object.keys(tree).forEach(moduleName => {
-		res.push({
-			name: moduleName,
-			text: moduleName,
-			url: `${config.baseUrl}${moduleName}`
-		});
-		const moduleTree = tree[moduleName];
-		// noinspection DuplicatedCode
-		moduleTree.forEach(moduleMember => {
-			res.push({
-				name: moduleMember.name,
-				text: moduleName + '.' + moduleMember.name,
-				url: `${config.baseUrl}${moduleName}#${moduleMember.name}`
-			});
-
-			for (const node of moduleMember.declarations) {
-				if (isClass(node)) {
-					const classDeclaration = node.declarations[0];
-					classDeclaration.properties?.forEach(property => {
-						res.push({
-							name: property.name,
-							text: `${moduleName}.${moduleMember.name}.${property.name}`,
-							url: `${config.baseUrl}${moduleName}#${moduleMember.name}.${property.name}`
-						});
-					});
-					classDeclaration.methods?.forEach(method => {
-						res.push({
-							name: method.name,
-							text: `${moduleName}.${moduleMember.name}.${method.name}`,
-							url: `${config.baseUrl}${moduleName}#${moduleMember.name}.${method.name}`
-						});
-					});
-				} else if (isInterface(node)) {
-					const interfaceDeclaration = node.declarations[0];
-					interfaceDeclaration.properties?.forEach(property => {
-						res.push({
-							name: property.name,
-							text: `${moduleName}.${moduleMember.name}.${property.name}`,
-							url: `${config.baseUrl}${moduleName}#${moduleMember.name}.${property.name}`
-						});
-					});
-					interfaceDeclaration.methods?.forEach(method => {
-						res.push({
-							name: method.name,
-							text: `${moduleName}.${moduleMember.name}.${method.name}`,
-							url: `${config.baseUrl}${moduleName}#${moduleMember.name}.${method.name}`
-						});
-					});
-				}
-			}
-		});
-	});
-
-	return res;
-}
-
-/**
- * Checks if the given node is a {@link ModuleTreeNode} contains a class declaration
- *
- * @param node - the node
- * @returns does `node.declarations` contain a class declaration?
- *
- * @example
- * ```ts
- * if (isClass(node)) {
- *     // node.declarations[0] is a ClassDeclarationStructure
- * }
- * ```
- */
-function isClass(
-	node: ModuleTreeNode
-): node is ModuleTreeNode<ClassDeclarationStructure> {
-	return node.type === 'class';
-}
-
-/**
- * Checks if the given node is a {@link ModuleTreeNode} contains an interface declaration
- *
- * @param node - the node
- * @returns does `node.declarations` contain an interface declaration?
- *
- * @example
- * ```ts
- * if (isInterface(node)) {
- *     // node.declarations[0] is an InterfaceDeclarationStructure
- * }
- * ```
- */
-function isInterface(
-	node: ModuleTreeNode
-): node is ModuleTreeNode<InterfaceDeclarationStructure> {
-	return node.type === 'interface';
-}
-
-/**
- * A search result.
- */
-export interface SearchResult {
-	name: string;
-	url: string;
-	text: string;
-}

commit 4041fc02a29eda29a4df7258a7448c294d524ce2

fix(html-theme): Fix broken links in search index introduced in be6baffca4bf663c9f6664397f319c659e162059

src/themes/html/search-index.ts
@@ -66,13 +66,13 @@ function getSearchIndexForModuleMembers(
 	moduleTree.forEach(moduleMember => {
 		res.push({
 			name: moduleMember.name,
-			text: moduleName + '.' + moduleMember.name,
+			text: moduleName + '#' + moduleMember.name,
 			url: `${config.baseUrl}${moduleName}#${moduleMember.name}`
 		});
 		res.push(
 			...getSearchIndexForModuleMemberChildren(
 				moduleMember.declarations,
-				moduleName + '.' + moduleMember.name,
+				moduleName + '#' + moduleMember.name,
 				config
 			)
 		);