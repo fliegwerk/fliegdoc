f85cda7205761a535166fba3679ba88f94eb35a7
Pablo Klaschka <contact@pabloklaschka.de>
2021-01-22 05:06:19 +0100

    refactor(html-theme): Decrease duplicate code by creating updateJSDocsForStructure.ts

--- src/processors/helpers/extractPropertiesAndMethods.ts
+++ src/processors/helpers/extractPropertiesAndMethods.ts
@@ -5,6 +5,7 @@ import {
 	InterfaceDeclarationStructure
 } from 'ts-morph';
 import { processJsDocs } from './processJsDocs';
+import { updateJSDocsForStructure } from './updateJSDocsForStructure';
 
 /**
  * Extracts docs for the node (and its structure), its root docs, as well as the structure of its properties and methods
@@ -29,18 +30,6 @@ export function extractPropertiesAndMethods(
 	node: ClassDeclaration | InterfaceDeclaration
 ): void {
 	structure.docs = processJsDocs(node.getJsDocs());
-	if (structure.properties) {
-		for (let i = 0; i < structure.properties.length; i++) {
-			structure.properties[i].docs = processJsDocs(
-				node.getProperties()[i].getJsDocs()
-			);
-		}
-	}
-	if (structure.methods) {
-		for (let i = 0; i < structure.methods.length; i++) {
-			structure.methods[i].docs = processJsDocs(
-				node.getMethods()[i].getJsDocs()
-			);
-		}
-	}
+	updateJSDocsForStructure(structure, node, 'properties', 'getProperties');
+	updateJSDocsForStructure(structure, node, 'methods', 'getMethods');
 }
